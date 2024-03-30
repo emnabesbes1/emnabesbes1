8153caed43dbd894fcfda9735e052e6dd87450d3
emnabesbes1 <160226418+emnabesbes1@users.noreply.github.com>
2024-03-30 23:38:08 +0100

    views.py

--- views.py
+++ views.py
@@ -1,44 +1,20 @@
-from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth import login, authenticate
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from .forms import UserRegistrationForm, UserLoginForm
 import requests
 import json
-from django.shortcuts import render
 from .models import Vulnerability
 from django.conf import settings
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+
+
+
+
 
 def BASE (request):
     return render(request,'base.html')
-
-
-def register(request):
-    if request.method == 'POST':
-        form = UserRegistrationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('login')
-    else:
-        form = UserRegistrationForm()
-    return render(request, 'registration/register.html', {'form': form})
-
-def user_login(request):
-    if request.method == 'POST':
-        form = UserLoginForm(request.POST)
-        if form.is_valid():
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password']
-            user = authenticate(request, username=username, password=password)
-            if user is not None:
-                login(request, user)
-                return redirect('dashboard')
-    else:
-        form = UserLoginForm()
-    return render(request, 'registration/login.html', {'form': form})
-
-def user_logout(request):
-    logout(request)
-    return redirect('login')
 
 
 def vulnerability_trends(request):
@@ -53,3 +29,32 @@ def new_scan(request):
     # Add any logic here for initiating a new scan
     return render(request, 'new_scan.html')
 
+ # Redirigez vers la page de connexion après la déconnexion
+
+
+
+def login_view(request):
+    error = None  # Initialisez la variable d'erreur à None
+
+    if request.method == 'POST':
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        print("User:", username)
+        print("User:", password)
+        # Vérifiez si l'utilisateur existe dans Django
+        User = authenticate(request, username=username, password=password)
+        print("User:",User)
+        
+        
+
+
+        if User is not None:
+            # Si l'utilisateur existe, connectez-le
+            login(request, User)
+            return redirect('base')  # Redirigez vers home
+        else:
+            # Si l'utilisateur n'existe pas, définissez un message d'erreur
+            error = 'INCORRECT USERNAME OR PASSWORD'
+
+    # Affichez la page de connexion avec un éventuel message d'erreur
+    return render(request, 'login.html', {'error': error})
