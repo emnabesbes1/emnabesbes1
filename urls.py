5b316423fb364dd6440110f01290563f9d869138
emnabesbes1 <160226418+emnabesbes1@users.noreply.github.com>
2024-03-30 23:40:02 +0100

    urls.py

--- urls.py
+++ urls.py
@@ -1,15 +1,13 @@
+from xml.etree.ElementInclude import include
+from django.urls import path, include 
+from . import views
 
-from django.contrib import admin
-from django.urls import path, include
-from django.conf.urls.static import static
-from django.conf import settings
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('pages.urls')),
+    path('', views.login_view, name='login'),
+    path('', views.BASE, name='BASE'),
+    path('new-scan/', views.new_scan, name='new-scan'),
+   
+    
 ]
-
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
